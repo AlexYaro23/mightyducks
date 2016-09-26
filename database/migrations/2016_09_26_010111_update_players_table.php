03ef205994f40e7f0028af78e4349d20009dc0c2
alex <ayaromenko@opera.com>
2016-09-26 04:23:39 +0300

    history playes

---
+++ database/migrations/2016_09_26_010111_update_players_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->tinyInteger('status')->unsigned()->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+}
