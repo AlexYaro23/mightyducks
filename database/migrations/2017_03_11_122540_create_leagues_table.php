c3ef1fba8c8294286ea04b792fa782773972c9bd
alex <ayaromenko@opera.com>
2017-03-12 00:59:28 +0200

    new images

---
+++ database/migrations/2017_03_11_122540_create_leagues_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeaguesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leagues', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('name');
+            $table->string('link')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->text('info')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('leagues');
+    }
+}
---
+++ database/migrations/2017_03_11_124924_update_tournaments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateTournamentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tournaments', function (Blueprint $table) {
+            $table->integer('league_id')->unsigned()->nullable()->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tournaments', function (Blueprint $table) {
+            $table->dropColumn('league_id');
+        });
+    }
+}
